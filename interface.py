4aeed9ef9358cbb47a0873d91ae91bee75227d50
AmyHulspas <amyhulspas@gmail.com>
2025-09-20 13:37:44 +0200

    Added a simple static plot

--- interface.py
+++ interface.py
@@ -15,8 +15,18 @@ def readMeasurement():
     #this will get replaced with the PID measurements
     return random.uniform(-1.0, 1.0)
 
-def plotMeasurements():
-    return
+def plotMeasurements(_window):
+    plot = _window.addPlot(title="measurements")
+    plot.setLabel("bottom", "Time")
+    plot.setLabel("left", "Measurements")
+
+    plotTime = list(range(1, 50))
+    #Pythonic way of iterating over plotTime and calling the function
+    plotValues = [readMeasurement() for t in plotTime] 
+
+    myPlot = plot.plot(plotTime, plotValues)
+
+    return _window
 
 def runInterface():
     #sys.arg is a required argument for QApplication,
@@ -24,6 +34,8 @@ def runInterface():
     pidInterfaceApp = QtWidgets.QApplication(sys.argv) #Create the application
     window = pg.GraphicsLayoutWidget(show=True, title="PID measurements") #Create the window
     window.show() #Show the window to the user
+
+    window = plotMeasurements(window)
 
     #This function essentially behaves like two seperate functions
     #First 'pidInterfaceApp.exec_()' runs the application and all it's logic
