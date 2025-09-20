6fde222adcd2820b572af07d682a296b05e7be1a
AmyHulspas <amyhulspas@gmail.com>
2025-09-20 15:55:21 +0200

    Realtime plot using randomized measurements

--- interface.py
+++ interface.py
@@ -10,38 +10,63 @@ import random
 import pyqtgraph as pg
 from PyQt5 import QtWidgets, QtCore
 
+measurements = []
+timeValues = []
+currentTime = 0
+plotReading = None
+plotWindow = None
+
+def update():
+    global currentTime
+
+    value = readMeasurement()
+    plotMeasurements(value, currentTime)
+    currentTime += 1
+
 def readMeasurement():
     #For now we just randomize a number,
     #this will get replaced with the PID measurements
     return random.uniform(-1.0, 1.0)
 
-def plotMeasurements(_window):
-    plot = _window.addPlot(title="measurements")
-    plot.setLabel("bottom", "Time")
-    plot.setLabel("left", "Measurements")
+def plotMeasurements(_value, _time):
 
-    plotTime = list(range(1, 50))
-    #Pythonic way of iterating over plotTime and calling the function
-    plotValues = [readMeasurement() for t in plotTime] 
+    measurements.append(_value)
+    timeValues.append(_time)
 
-    myPlot = plot.plot(plotTime, plotValues)
+    if len(measurements) > 100:
+        measurements.pop(0)
+        timeValues.pop(0)
 
-    return _window
+    # Update the plot
+    if plotReading is not None:
+        plotReading.setData(timeValues, measurements)
 
 def runInterface():
+    global plotWindow, plotReading, measurements, timeValues
+
     #sys.arg is a required argument for QApplication,
     #it allows sys.argv to read arguments from the command line 
     pidInterfaceApp = QtWidgets.QApplication(sys.argv) #Create the application
-    window = pg.GraphicsLayoutWidget(show=True, title="PID measurements") #Create the window
-    window.show() #Show the window to the user
+    plotWindow = pg.GraphicsLayoutWidget(show=True, title="PID measurements") #Create the window
+    #plotWindow.show() #Show the window to the user
 
-    window = plotMeasurements(window)
+    plot = plotWindow.addPlot(title="Real time measurements")
+    plot.setMouseEnabled(x=False, y=False)
+    plot.showGrid(x=True, y=True)
+    plot.setLabel("bottom", "Mesaurements")
+    plot.setLabel("left", "Time")
+
+    plotReading = plot.plot()
+
+    #Call the update function
+    timer = QtCore.QTimer()
+    timer.timeout.connect(update)
+    timer.start(10)
 
     #This function essentially behaves like two seperate functions
     #First 'pidInterfaceApp.exec_()' runs the application and all it's logic
     #When the application window is closed, 
     #'sys.exit' makes sure the application exits cleanly.
     sys.exit(pidInterfaceApp.exec_())
-
 
 runInterface()
