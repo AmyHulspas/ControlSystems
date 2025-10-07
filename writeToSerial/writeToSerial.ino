e6e6e20e32fbf5dcd6e70de9234938a490a81bbb
AmyHulspas <amyhulspas@gmail.com>
2025-10-07 18:58:08 +0200

    Calculate voltage from distance

--- writeToSerial/writeToSerial.ino
+++ writeToSerial/writeToSerial.ino
@@ -3,50 +3,48 @@ const int PIN_TRIGGER_DS = 32;
 const int PIN_ECHO_DS = 33;
 const int PIN_DAC = 25;
 
-const long minDistance = 2;
-const long maxDistance = 400;
+const float minDistance = 2;
+const float maxDistance = 50;
 
 void setup() {
   Serial.begin(115200);
-  
-  pinMode(PIN_SETPOINT, INPUT); //Setpoint pin
-  
-  //Distance sensor pin
+
+  // Distance sensor pins
   pinMode(PIN_TRIGGER_DS, OUTPUT);
   pinMode(PIN_ECHO_DS, INPUT);
 }
 
 float readDistanceCentimeters() {
-  //Make sure the pulse is low before sending a pulse
   digitalWrite(PIN_TRIGGER_DS, LOW);
-  delayMicroseconds(2);  // Wait briefly so the LOW state is stable
+  delayMicroseconds(2);
 
-  //Send out an 8 cycle sonic burst, we can calculate the distance based on how long they traveled
   digitalWrite(PIN_TRIGGER_DS, HIGH);
-  delayMicroseconds(10); // Keep it HIGH for at least 10 microseconds (sensor requirement)
+  delayMicroseconds(10);
   digitalWrite(PIN_TRIGGER_DS, LOW);
 
-  //The pulseIn() method measures how long the waves traveled in microseconds
-  long duration = pulseIn(PIN_ECHO_DS, HIGH, 30000);  //Timeout after 30ms if nothing is heard
+  long duration = pulseIn(PIN_ECHO_DS, HIGH, 30000); // 30 ms timeout
+  if (duration == 0) return -1.0; // no reading
 
-  //If there is no pulse, we exit the function
-  if (duration == 0) return -1;
-
-  //We calculate the distance of an object based on how long it took for the sound to get there
-  //0.0343 cm/us is the speed of sound, we then half the result to get the distance traveled one-way
-  float distanceCentimeters = (duration * 0.0343) / 2.0;
+  float distanceCentimeters = (duration * 0.0343f) / 2.0f;
   return distanceCentimeters;
 }
 
-float convertDistanceToDac(long _distance) {
-  //Map the output from the distance sensor to a range between 0 and 255 (8-bit)
-  int dacValue = map(_distance, minDistance, maxDistance, 0, 255);
+void convertDistanceToDac(float _distance) {
+  ///If there is no correct distance value, we want to stop the function here as a fallback
+  if (_distance < 0) {
+    dacWrite(PIN_DAC, 0);
+    return;
+  }
 
-  //Clamp the value to 0-255, as a fallback, as a fallback
-  dacValue = constrain(dacValue, 0, 255);
+  //Calculate DAC output using gain variable style
+  //This value should fall in the range of 0-255
+  float gain = 255.0f / (maxDistance - minDistance);
+  float value = (_distance - minDistance) * gain;
 
-  //Convert the 8-bit values to a voltage between 0-3.3V
-  //Note: A DAC pin must be used (GPIO 25 or GPIO 26)
+  //But in case it doesn't, clamp it between 0 and 255 as a fallback
+  int dacValue = constrain(round(value), 0, 255);
+
+  //Convert the 0-255 value to a voltage output between 0-3.3V
   dacWrite(PIN_DAC, dacValue);
 }
 
@@ -54,14 +52,14 @@ void loop() {
   float distanceCentimeters = readDistanceCentimeters();
   int setpointRaw = analogRead(PIN_SETPOINT);
 
+  convertDistanceToDac(distanceCentimeters); // update DAC output
+
   //If there is distance reading, send the distance and setpoint values to the serial, so we can read it in Python
   if (distanceCentimeters >= 0) {
     Serial.print(distanceCentimeters, 2);  // send distance (2 decimal places)
     Serial.print(",");
     Serial.println(setpointRaw); // send raw ADC reading
   }
-
-  convertDistanceToDac(float(distanceCentimeters)); //Convert the distance to a voltage output between 0-3.3V
 
   delay(50);
 }
